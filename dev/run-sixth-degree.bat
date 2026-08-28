d7c1052cd58507d6cf6d1ce52d8c6c91d8c8ce1e
ZoltanPepper <78757822+ZoltanPepper@users.noreply.github.com>
2026-08-28 13:44:54 +0100

    Use RuneLite bundled Java for owner test launcher
    * Use RuneLite bundled Java for dev launcher
    
    * Simplify RuneLite dev test instructions

--- dev/run-sixth-degree.bat
+++ dev/run-sixth-degree.bat
@@ -2,21 +2,28 @@
 setlocal
 cd /d "%~dp0"
 
+set "JAVA_CMD=%LOCALAPPDATA%\RuneLite\jre\bin\java.exe"
+if exist "%JAVA_CMD%" goto java_ready
+
 where java >nul 2>nul
 if errorlevel 1 (
   echo.
-  echo Java 11 or newer is required to run this development build.
-  echo Install a Java 11+ runtime, then run this file again.
+  echo Could not find RuneLite's bundled Java or a system Java runtime.
+  echo Make sure the normal RuneLite Windows launcher is installed, then try again.
+  echo Expected RuneLite Java path:
+  echo %LOCALAPPDATA%\RuneLite\jre\bin\java.exe
   echo.
   pause
   exit /b 1
 )
+set "JAVA_CMD=java"
 
+:java_ready
 echo Starting RuneLite with the Sixth Degree development plugin...
-echo This uses the normal RuneLite client in developer mode.
+echo Java: %JAVA_CMD%
 echo.
 
-java -ea -cp "app\sixth-degree-runelite-dev.jar;app\lib\*" com.sixthdegree.SixthDegreePluginTest --developer-mode --debug
+"%JAVA_CMD%" -ea -cp "app\sixth-degree-runelite-dev.jar;app\lib\*" com.sixthdegree.SixthDegreePluginTest --developer-mode --debug
 set EXIT_CODE=%ERRORLEVEL%
 
 if not "%EXIT_CODE%"=="0" (
